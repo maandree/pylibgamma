efdef7cd34ad9070b92cf806d2a44ba8a6bbb8bb
Mattias Andrée <maandree@kth.se>
2024-06-02 09:36:48 +0200

    Fix makefile
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- config.mk
+++ config.mk
@@ -5,8 +5,8 @@ PYTHON_MINOR = $$(python$(PYTHON_MAJOR) --version 2>&1 | cut -d . -f 2)
 
 PYTHON_VERSION = $(PYTHON_MAJOR).$(PYTHON_MINOR)
 PYTHON_VER     = $(PYTHON_MAJOR)$(PYTHON_MINOR)
-PYTHON_DIR     = $(PREFIX)/lib/python$(python_version)/site-packages
-PYTHON_CACHE   = $(python_dir)/__pycache__
+PYTHON_DIR     = $(PREFIX)/lib/python$(PYTHON_VERSION)/site-packages
+PYTHON_CACHE   = $(PYTHON_DIR)/__pycache__
 
 CC     = c99
 CYTHON = cython$(PYTHON_MAJOR)
